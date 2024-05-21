b4f18044e205f7faeec495470321c151244de374
Devjonasbt <jonas.bcruz@senacsp.edu.br>
2024-05-20 23:18:06 -0300

    complemento

--- script.js
+++ script.js
@@ -184,7 +184,7 @@ function checkRestaurantOpen() {
     const data = new Date();
     const hora = data.getHours();
 
-    return hora >= 18 && hora < 22;
+    return hora >= 18 && hora < 23;
 }
 
 const spanItem = document.getElementById('date-span');
