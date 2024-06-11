3516690ead803894793976a4800a055a011dba79
Devjonasbt <129806694+Devjonasbt@users.noreply.github.com>
2024-06-11 20:07:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const menu = document.getElementById('menu');
 const cartBtn = document.getElementById('cart-btn');
-
 const cartModal = document.getElementById('cart-modal');
 const cartItemsModal = document.getElementById('cart-items');
 const cartTotalModal = document.getElementById('cart-total');
@@ -9,55 +8,51 @@ const closeBtnModal = document.getElementById('close-modal-btn');
 const addressInputModal = document.getElementById('address');
 const addressWarnModal = document.getElementById('address-warn');
 const orderDescriptionInput = document.getElementById('order-description');
-
 const cartCounter = document.getElementById('cart-count');
 
 // Lista de itens do carrinho
 let cartList = [];
 
-// Abre o modal do carrinho
-cartBtn.addEventListener('click', function () {
-    updateCartModal();
-    cartModal.style.display = 'flex';
-});
+// Verifique a existência dos elementos antes de adicionar eventos
+if (cartBtn) {
+    cartBtn.addEventListener('click', function () {
+        updateCartModal();
+        cartModal.style.display = 'flex';
+    });
+}
 
-// Fecha modal quando clica fora
-cartModal.addEventListener('click', function (event) {
-    if (event.target === cartModal) {
+if (cartModal) {
+    cartModal.addEventListener('click', function (event) {
+        if (event.target === cartModal) {
+            cartModal.style.display = 'none';
+        }
+    });
+}
+
+if (closeBtnModal) {
+    closeBtnModal.addEventListener('click', function () {
         cartModal.style.display = 'none';
-    }
-});
+    });
+}
 
-closeBtnModal.addEventListener('click', function () {
-    cartModal.style.display = 'none';
-});
-
-// Recebe o nome, valor e adiciona ao carrinho ao clicar no botão ou no ícone 
-menu.addEventListener('click', function (event) {
-    let parentButton = event.target.closest('.add-to-cart-btn');
-
-    if (parentButton) {
-        const name = parentButton.getAttribute('data-name');
-        const price = parseFloat(parentButton.getAttribute('data-price'));
-
-        // Adicionando no carrinho
-        addToCart(name, price);
-    }
-});
+if (menu) {
+    menu.addEventListener('click', function (event) {
+        const parentButton = event.target.closest('.add-to-cart-btn');
+        if (parentButton) {
+            const name = parentButton.getAttribute('data-name');
+            const price = parseFloat(parentButton.getAttribute('data-price'));
+            addToCart(name, price);
+        }
+    });
+}
 
 function addToCart(name, price) {
     const existingItem = cartList.find(item => item.name === name);
-
     if (existingItem) {
         existingItem.quantity += 1;
     } else {
-        cartList.push({
-            name,
-            price,
-            quantity: 1,
-        });
+        cartList.push({ name, price, quantity: 1 });
     }
-
     updateCartModal();
     updateCartCounter();
 }
@@ -77,16 +72,17 @@ function updateCartModal() {
                     <p>Qtd: ${item.quantity}</p>
                     <p class='font-medium mt-2'>R$ ${item.price.toFixed(2)}</p>
                 </div>
-                <button class="remove-from-cart-btn" data-name="${item.name}"> 
+                <button class="remove-from-cart-btn" data-name="${item.name}">
                     Remover
                 </button>
             </div>
         `;
-        total += item.price * item.quantity + 5;
-
+        total += item.price * item.quantity;
         cartItemsModal.appendChild(cartItemElement);
     });
 
+    const deliveryFee = 5.00;
+    total += deliveryFee;
     cartTotalModal.textContent = total.toLocaleString('pt-BR', {
         style: 'currency',
         currency: 'BRL'
@@ -98,20 +94,19 @@ function updateCartCounter() {
     cartCounter.innerHTML = itemCount;
 }
 
-cartItemsModal.addEventListener('click', function (event) {
-    if (event.target.classList.contains('remove-from-cart-btn')) {
-        const name = event.target.getAttribute('data-name');
-
-        removeItemCart(name);
-    }
-});
+if (cartItemsModal) {
+    cartItemsModal.addEventListener('click', function (event) {
+        if (event.target.classList.contains('remove-from-cart-btn')) {
+            const name = event.target.getAttribute('data-name');
+            removeItemCart(name);
+        }
+    });
+}
 
 function removeItemCart(name) {
     const index = cartList.findIndex(item => item.name === name);
-
     if (index !== -1) {
         const item = cartList[index];
-
         if (item.quantity > 1) {
             item.quantity -= 1;
         } else {
@@ -122,127 +117,88 @@ function removeItemCart(name) {
     }
 }
 
-addressInputModal.addEventListener('input', function (event) {
-    let inputValue = event.target.value;
-
-    if (inputValue !== '') {
-        addressInputModal.classList.remove('border-red-500');
-        addressWarnModal.classList.add('hidden');
-    }
-});
-
-// Finalizar pedido
-checkoutBtnModal.addEventListener('click', function () {
-    // Verifique se o carrinho está vazio
-    if (cartList.length === 0) {
-        // Mostrar uma mensagem para o usuário
-        Toastify({
-            text: "Seu carrinho está vazio. Adicione itens antes de finalizar o pedido.",
-            duration: 3000,
-            close: true,
-            gravity: "top",
-            position: "left",
-            backgroundColor: "linear-gradient(to right, #ff4e50, #f9d423)",
-        }).showToast();
-        
-        return;
-    }
-
-    // Verifique se o nome do usuário está preenchido
-    const userName = document.getElementById('user-name').value.trim();
-    if (userName === '') {
-        Toastify({
-            text: "Por favor, digite seu nome!",
-            backgroundColor: "linear-gradient(to right, #ff4e50, #f9d423)",
-            duration: 3000
-        }).showToast();
-        return;
-    }
-
-    // Verifique se o endereço de entrega está preenchido
-    if (addressInputModal.value === '') {
-        addressWarnModal.classList.remove('hidden');
-        addressInputModal.classList.add('border-red-500');
-        return;
-    }
-
-    // Verifique qual opção de pagamento foi selecionada
-    let formaPagamento = '';
-    const formasPagamento = document.querySelectorAll('input[name="forma_pagamento"]');
-    formasPagamento.forEach(opcao => {
-        if (opcao.checked) {
-            formaPagamento = opcao.value;
+if (addressInputModal) {
+    addressInputModal.addEventListener('input', function (event) {
+        const inputValue = event.target.value;
+        if (inputValue !== '') {
+            addressInputModal.classList.remove('border-red-500');
+            addressWarnModal.classList.add('hidden');
         }
     });
+}
 
-    // Verifique se uma forma de pagamento foi selecionada
-    if (formaPagamento === '') {
-        Toastify({
-            text: "Por favor, selecione uma forma de pagamento!",
-            backgroundColor: "linear-gradient(to right, #ff4e50, #f9d423)",
-            duration: 3000
-        }).showToast();
-        return;
-    }
+if (checkoutBtnModal) {
+    checkoutBtnModal.addEventListener('click', function () {
+        if (cartList.length === 0) {
+            showToast("Seu carrinho está vazio. Adicione itens antes de finalizar o pedido.", "#ff4e50");
+            return;
+        }
 
-    // Verifique se o restaurante está aberto
-    const isOpen = checkRestaurantOpen();
-    if (!isOpen) {
-        Toastify({
-            text: "Ops!! Restaurante fechado no momento",
-            duration: 3000,
-            destination: "https://github.com/Devjonasbt/JB_Sushi.git",
-            close: true,
-            gravity: "top",
-            position: "left",
-            stopOnFocus: true,
-            style: {
-                background: "#ef4444",
-            },
-        }).showToast();
-        return;
-    }
+        const userName = document.getElementById('user-name').value.trim();
+        if (userName === '') {
+            showToast("Por favor, digite seu nome!", "#ff4e50");
+            return;
+        }
 
-    // Taxa de entrega fixa
-    const deliveryFee = 5.00;
+        if (addressInputModal.value === '') {
+            addressWarnModal.classList.remove('hidden');
+            addressInputModal.classList.add('border-red-500');
+            return;
+        }
 
-    // Obtenha a descrição do pedido
-    const orderDescription = orderDescriptionInput.value.trim();
+        let formaPagamento = '';
+        const formasPagamento = document.querySelectorAll('input[name="forma_pagamento"]');
+        formasPagamento.forEach(opcao => {
+            if (opcao.checked) {
+                formaPagamento = opcao.value;
+            }
+        });
 
-    // Mapeie os itens do carrinho para uma string formatada
-    const cartItems = cartList.map((item) => {
-        return `${item.quantity} * ${item.name} - R$${item.price.toFixed(2)}`;
-    }).join("%0A");
+        if (formaPagamento === '') {
+            showToast("Por favor, selecione uma forma de pagamento!", "#ff4e50");
+            return;
+        }
 
-    // Codifique o endereço de entrega
-    const address = encodeURIComponent(addressInputModal.value);
-    const phone = '5511913620016';
+        if (!checkRestaurantOpen()) {
+            showToast("Ops!! Restaurante fechado no momento", "#ef4444");
+            return;
+        }
 
-    // Calcule o total do pedido
-    const total = cartList.reduce((acc, item) => acc + item.price * item.quantity, 0);
-    const totalFormatted = total.toLocaleString('pt-BR', {
-        style: 'currency',
-        currency: 'BRL'
+        const deliveryFee = 5.00;
+        const orderDescription = orderDescriptionInput.value.trim();
+        const cartItems = cartList.map(item => `${item.quantity} * ${item.name} - R$${item.price.toFixed(2)}`).join("%0A");
+        const address = encodeURIComponent(addressInputModal.value);
+        const phone = '5511913620016';
+        const total = cartList.reduce((acc, item) => acc + item.price * item.quantity, 0);
+        const totalFormatted = total.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        });
+
+        const descriptionText = orderDescription ? `%0A%0ADescrição do pedido: ${encodeURIComponent(orderDescription)}` : '';
+        const message = `Cliente: ${userName}%0A%0AOla! Aqui esta os detalhes do meu pedido:%0A%0A${cartItems}${descriptionText}%0A%0ASubTotal:${totalFormatted}%0ATaxa de entrega: R$${deliveryFee.toFixed(2)}%0ATotal: R$${(total + deliveryFee).toFixed(2)}%0A%0AForma de pagamento: ${formaPagamento}%0A%0AEndereço de entrega:%0A${address}%0A%0AMuito obrigado!%0A%0A`;
+
+        window.open(`https://wa.me/${phone}?text=${message}`, "_blank");
+
+        cartList = [];
+        updateCartModal();
+        updateCartCounter();
     });
-
-    // Adicione a descrição do pedido à mensagem, se houver
-    const descriptionText = orderDescription ? `%0A%0ADescrição do pedido: ${encodeURIComponent(orderDescription)}` : '';
-
-    // Adicione o nome do usuário à mensagem
-    const message = `Cliente: ${userName}%0A%0AOla! Aqui esta os detalhes do meu pedido:%0A%0A${cartItems}${descriptionText}%0A%0ASubTotal:${totalFormatted}%0ATaxa de entrega: R$${deliveryFee.toFixed(2)}%0ATotal: R$${(total + deliveryFee).toFixed(2)}%0A%0AForma de pagamento: ${formaPagamento}%0A%0AEndereço de entrega:%0A${address}%0A%0AMuito obrigado!%0A%0A`;
-
-    // Abra o link do WhatsApp com a mensagem
-    window.open(`https://wa.me/${phone}?text=${message}`, "_blank");
-
-    // Limpe o carrinho e atualize o modal e o contador do carrinho
-    cartList = [];
-    updateCartModal();
-    updateCartCounter();
-});
+}
 
 function checkRestaurantOpen() {
-    // Sempre retornar verdadeiro, indicando que o restaurante está sempre aberto
-    return true;
+    return true; // Implementar lógica de verificação do horário de funcionamento aqui se necessário.
+}
+
+function showToast(message, color) {
+    Toastify({
+        text: message,
+        duration: 3000,
+        close: true,
+        gravity: "top",
+        position: "left",
+        backgroundColor: color,
+    }).showToast();
 }
 
 const spanItem = document.getElementById('date-span');
@@ -254,4 +210,4 @@ if (isOpen) {
 } else {
     spanItem.classList.remove('bg-green-600');
     spanItem.classList.add('bg-red-500');
-}
+                }
